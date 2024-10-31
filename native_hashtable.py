1c182ef7a1783912fc0f6fcff13636e1f939be28
Sharad <sharad@mineo333.dev>
2024-10-31 19:44:53 -0400

    finish trygetmetadata

--- native_hashtable.py
+++ native_hashtable.py
@@ -539,7 +539,10 @@ class Handle:
     
     @property
     def Offset(self):
-        return self.value & 0xffffff     
+        return self.value & 0xffffff   
+    
+    def AsInt():
+        return value
 
 #https://github.com/dotnet/runtime/blob/87fea60432fb34a2537a3a593c80042d8230b986/src/mono/System.Private.CoreLib/src/System/RuntimeTypeHandle.cs#L41
 class RuntimeTypeHandle:
@@ -613,14 +616,20 @@ class TypeLoaderEnvironment:
                 # I think we can just pass entryMetadataHandle directly into HandleType
                 # https://github.com/dotnet/runtime/blob/f72784faa641a52eebf25d8212cc719f41e02143/src/coreclr/tools/Common/Internal/Metadata/NativeFormat/NativeMetadataReader.cs#L107
                 if entryMetadataHandle.HandleType == HandleType.TypeDefinition:
-                    metadataReader = NativeReader() # Change this to a MetadataReader
-                    return QTypeDefinition(metadataReader, entryMetadataHandle)
+                    metadataReader = METADATA_READER # Change this to a MetadataReader
+                    return (True, QTypeDefinition(metadataReader, entryMetadataHandle))
+
+        
+        return (False, None)
+
                     
 
 class QTypeDefinition:
     def __init__(self, reader, handle):
         self.reader = reader
-        self.handle = handle #int
+        self.handle = handle.AsInt()
+    
+    
 
 # pulled from: https://github.com/dotnet/runtime/blob/a72cfb0ee2669abab031c5095a670678fd0b7861/src/coreclr/tools/Common/Internal/Metadata/NativeFormat/NativeFormatReaderGen.cs#L3221
 class MethodHandle:
@@ -683,11 +692,6 @@ class MetadataReader:
         def isNull(self, handle):
             return handle.value == NullHandle.value
 
-        def ToHandle(self, handle):
-            return handle  
-
-        def StringEquals(self, handle, value):
-            pass
 
 # MAIN PARSING CODE STARTS HERE
             
@@ -696,7 +700,8 @@ class MetadataReader:
 def create_metadata_reader(): 
     global METADATA_READER
     (metadata_start, metadata_end) = find_section_start_end(ReflectionMapBlob.EmbeddedMetadata)  
-    metadataNativeReader = NativeReader(metadata_start, metadata_end-metadata_start)
+    #metadataNativeReader = NativeReader(metadata_start, metadata_end-metadata_start)
+    METADATA_READER = MetadataReader(metadata_start, metadata_end-metadata_start)
     
 
 #this comes from here: https://github.com/dotnet/runtime/blob/c43fc8966036678d8d603bdfbd1afd79f45b420b/src/coreclr/nativeaot/System.Private.Reflection.Execution/src/Internal/Reflection/Execution/ExecutionEnvironmentImplementation.MappingTables.cs#L643
@@ -734,6 +739,7 @@ def parse_hashtable(invokeMapStart, invokeMapEnd):
 
 initialize_types()
 initialize_rtr()
+create_metadata_reader()
 (start,end) = find_section_start_end(ReflectionMapBlob.InvokeMap)
 print('__method_entrypoint_map start:', hex(start), 'end:', hex(end))
 parse_hashtable(start, end)
