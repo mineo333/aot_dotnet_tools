0567636a7c46288f7330d89125c0e6c83b8088ec
hl4x <hl4x@proton.me>
2024-11-06 19:20:13 -0500

    reformat rehydrate

--- rehydrate.py
+++ rehydrate.py
@@ -3,37 +3,38 @@ import struct
 import ctypes
 from .rtr import *
 
-#pulled from: https://github.com/dotnet/runtime/blob/d450d9c9ee4dd5a98812981dac06d2f92bdb8213/src/coreclr/tools/Common/Internal/Runtime/DehydratedData.cs#L64
-
-DehydratedDataCommandMask = 0x7
-DehydratedDataCommandPayloadShift = 0x3
-MaxRawShortPayload = (1 << (8 - DehydratedDataCommandPayloadShift)) - 1
-MaxExtraPayloadBytes = 3
-MaxShortPayload = MaxRawShortPayload - MaxExtraPayloadBytes
-
-Copy = 0x00
-ZeroFill = 0x01
-RelPtr32Reloc = 0x02
-PtrReloc = 0x03
-InlineRelPtr32Reloc = 0x04
-InlinePtrReloc = 0x05
-#END CONSTANTS
-
-
-def parse_command(br):
-    b = br.read8()
-    command = b & DehydratedDataCommandMask
-    payload = b >> DehydratedDataCommandPayloadShift
-    extra_bytes = payload - MaxShortPayload
-    if extra_bytes > 0:
-        payload = br.read8()
-        if extra_bytes > 1:
-            payload += (br.read8() << 8)
-            if extra_bytes > 2:
-                payload += (br.read8() << 16)
-        payload += MaxShortPayload
+#https://github.com/dotnet/runtime/blob/d450d9c9ee4dd5a98812981dac06d2f92bdb8213/src/coreclr/tools/Common/Internal/Runtime/DehydratedData.cs#L20
+class DehydratedDataCommand:
+    def __init__(self):
+        pass
     
-    return (payload, command)
+    Copy = 0x00
+    ZeroFill = 0x01
+    RelPtr32Reloc = 0x02
+    PtrReloc = 0x03
+    InlineRelPtr32Reloc = 0x04
+    InlinePtrReloc = 0x05
+
+    DehydratedDataCommandMask = 0x7
+    DehydratedDataCommandPayloadShift = 0x3
+    MaxRawShortPayload = (1 << (8 - DehydratedDataCommandPayloadShift)) - 1
+    MaxExtraPayloadBytes = 3
+    MaxShortPayload = MaxRawShortPayload - MaxExtraPayloadBytes
+
+    def Decode(self, pB):
+        b = pB.read8()
+        command = b & self.DehydratedDataCommandMask
+        payload = b >> self.DehydratedDataCommandPayloadShift
+        extra_bytes = payload - self.MaxShortPayload
+        if extra_bytes > 0:
+            payload = pB.read8()
+            if extra_bytes > 1:
+                payload += (pB.read8() << 8)
+                if extra_bytes > 2:
+                    payload += (pB.read8() << 16)
+            payload += self.MaxShortPayload
+        
+        return (payload, command)
 
 def ReadRelPtr32(br):
     #print(hex(br.offset))
@@ -43,37 +44,43 @@ def WriteRelPtr32(bw, value):
     return bw.write32(value-bw.offset)
 
 #reimplement the following algorithm: https://github.com/dotnet/runtime/blob/a3fe47ef1a8def24e8d64c305172199ae5a4ed07/src/coreclr/nativeaot/Common/src/Internal/Runtime/CompilerHelpers/StartupCodeHelpers.cs#L247
-def rehydrate_data(bv, start, length):
-    
+def RehydrateData(bv, start, length):
     pEnd = start+length
-    br = bv.reader(start) #br.offset is pCurrent
-    fixup_br = bv.reader(pEnd) #fixup_br is for reading from pFixups
-    bw = bv.writer(ReadRelPtr32(br)) #bw.offset is pDest
+    pCurrentReader = bv.reader(start) #pCurrentReader.offset is pCurrent
+    FixupBr = bv.reader(pEnd) #FixupBr is for reading from pFixups
+    pDestReader = bv.writer(ReadRelPtr32(pCurrentReader)) #pDestReader.offset is pDest
+
     bv.memory_map.remove_memory_region('hydrated_mem')
-    assert bv.memory_map.add_memory_region('hydrated_mem', bw.offset, b'\x00'*length)
-    while br.offset < pEnd:
-        (payload, command) = parse_command(br)
+    assert bv.memory_map.add_memory_region('hydrated_mem', pDestReader.offset, b'\x00'*length)
+
+    while pCurrentReader.offset < pEnd:
+        (payload, command) = DehydratedDataCommand.Decode(DehydratedDataCommand, pCurrentReader)
         #print(hex(br.offset))
-        if command == Copy:
-            data = br.read(payload)
-            bw.write(data)
-        elif command == ZeroFill:
-            bw.seek_relative(payload)
-        elif command == PtrReloc:
-            fixup_br.seek(pEnd + payload*4)
-            val = ReadRelPtr32(fixup_br)
-            bw.write64(val)
-        elif command == RelPtr32Reloc:
-            fixup_br.seek(pEnd + payload*4)
-            WriteRelPtr32(bw, ReadRelPtr32(fixup_br))
-        elif command == InlinePtrReloc:
-            for i in range(payload):
-                val = ReadRelPtr32(br)
-                bw.write64(val)
-        elif command == InlineRelPtr32Reloc:
-            for i in range(payload):
-                WriteRelPtr32(bw, ReadRelPtr32(br))
-    
+        match command:
+            case DehydratedDataCommand.Copy:
+                data = pCurrentReader.read(payload)
+                pDestReader.write(data)
+
+            case DehydratedDataCommand.ZeroFill:
+                pDestReader.seek_relative(payload)
+
+            case DehydratedDataCommand.PtrReloc:
+                FixupBr.seek(pEnd + payload*4)
+                val = ReadRelPtr32(FixupBr)
+                pDestReader.write64(val)
+
+            case DehydratedDataCommand.RelPtr32Reloc:
+                FixupBr.seek(pEnd + payload*4)
+                WriteRelPtr32(pDestReader, ReadRelPtr32(FixupBr))
+
+            case DehydratedDataCommand.InlinePtrReloc:
+                for i in range(payload):
+                    val = ReadRelPtr32(pCurrentReader)
+                    pDestReader.write64(val)
+
+            case DehydratedDataCommand.InlineRelPtr32Reloc:
+                for i in range(payload):
+                    WriteRelPtr32(pDestReader, ReadRelPtr32(pCurrentReader))
         
 def detect_pointers(bv): #this function works best rebased
     br = bv.reader(bv.sections['hydrated'].start)
@@ -86,7 +93,7 @@ def detect_pointers(bv): #this function works best rebased
 
 def do_rehydration(bv):
     (start, end) = find_section_start_end(ReadyToRunSectionType.DehydratedData)
-    rehydrate_data(bv, start, end-start)
+    RehydrateData(bv, start, end-start)
     detect_pointers(bv)
 
 
